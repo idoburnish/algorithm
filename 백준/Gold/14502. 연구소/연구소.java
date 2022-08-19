12c1b858094b9ffe14056b0be719488f838d5e75
LEE DOYOON <doyun7433@naver.com>
2022-08-19 15:01:28 +0900

    [Gold IV] Title: 연구소, Time: 880 ms, Memory: 297536 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -2,19 +2,20 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int N, M, answer = 0;
-    static int[][] map;
+    public static int[][] map;
+    public static int N, M, answer = Integer.MIN_VALUE;
+
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
 
-    public static void DFS(int cnt) {
+    public static void DFS(int cnt){
         if (cnt == 3) BFS();
         else {
             for (int i=0; i<N; i++) {
                 for (int j=0; j<M; j++) {
                     if (map[i][j] == 0) {
                         map[i][j] = 1;
-                        DFS(cnt + 1);
+                        DFS(cnt+1);
                         map[i][j] = 0;
                     }
                 }
@@ -59,19 +60,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());   // 세로
+        M = Integer.parseInt(st.nextToken());   // 가로
 
         map = new int[N][M];
         for (int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j=0; j<M; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
+            for (int j=0; j<M; j++) map[i][j] = Integer.parseInt(st.nextToken());
         }
 
         DFS(0);
-
         System.out.println(answer);
     }
 }
