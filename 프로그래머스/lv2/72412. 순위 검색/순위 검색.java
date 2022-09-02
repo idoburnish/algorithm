45ab6d462437ed5258c8e259e9dccd378dea873f
LEE DOYOON <doyun7433@naver.com>
2022-09-02 14:43:05 +0900

    [level 2] Title: 순위 검색, Time: 1015.71 ms, Memory: 222 MB -BaekjoonHub

--- 프로그래머스/lv2/72412. 순위 검색/순위 검색.java
+++ 프로그래머스/lv2/72412. 순위 검색/순위 검색.java
@@ -1,75 +1,65 @@
 import java.util.*;
 
-
-class Solution {
-    static ArrayList<ArrayList<Integer>> list=new ArrayList<>();    
-    static Map<String,List<Integer>> map=new HashMap<>();
+class Solution {    
     static int[] answer;
+    static HashMap<String, ArrayList<Integer>> map = new HashMap<>();
     
-    static void dfs(String str,int depth,String[] info){
-        if(depth==4){
-            if(map.containsKey(str)==false){
-                List<Integer> list=new ArrayList<>();
-                list.add(Integer.parseInt(info[4]));
-                map.put(str,list);
-            }else{
-                map.get(str).add(Integer.parseInt(info[4]));
-            }
-            return;
+    public void setInfo(String[] info) {
+        for (String str : info) {
+            String[] temp = str.split(" ");
+            DFS(temp, "", 0);
         }
         
-        dfs(str+"-",depth+1,info);
-        dfs(str+info[depth],depth+1,info);
-    }
-    
-    static void setInfo(String[] info){
-        for(int i=0;i<info.length;i++){
-            dfs("",0,info[i].split(" "));
-        }
-        
-        Iterator<String> it= map.keySet().iterator();
-        while(it.hasNext()){
-            String key=it.next();
-            List<Integer> li=map.get(key);
-            Collections.sort(li);
+        for (String str : map.keySet()) {
+            ArrayList<Integer> temp = map.get(str);
+            Collections.sort(temp);
         }
     }
     
-    static int counting(String str,int score){
-        if(map.containsKey(str)==false) return 0;
-        
-        List<Integer> list=map.get(str);
-        int start=0,end=list.size()-1;
-        
-        while(start<=end){
-            int mid=(start+end)/2;
-            if(list.get(mid)<score){
-                start=mid+1;
-            }else{
-                end=mid-1;
-            }
+    public void DFS(String[] info, String infoString, int cnt) {
+        if (cnt == 4) {
+            ArrayList<Integer> arr = map.getOrDefault(infoString, new ArrayList<>());
+            arr.add(Integer.parseInt(info[4]));
+            map.put(infoString, arr);
         }
-        
-        return list.size()-start;
+        else {
+            DFS(info, infoString + info[cnt], cnt + 1);
+            DFS(info, infoString + "-", cnt + 1);
+        }
     }
     
-    static void makeAnswer(String[] query){
-        for(int i=0;i<query.length;i++){
-            String str="";
-            String[] arr=query[i].split(" ");
+    public void queryProcess(String[] query) {
+        for (int i=0; i<query.length; i++) {
+            String[] temp = query[i].split(" ");
             
-            for(int j=0;j<arr.length-1;j++){
-                if(arr[j].equals("and")) continue; 
-                str+=arr[j];
+            String q = "";
+            for (int k=0; k<temp.length-1; k++) {
+                if (!temp[k].equals("and")) q += temp[k];
             }
-            answer[i]=counting(str,Integer.parseInt(arr[arr.length-1]));
+            
+            answer[i] = getAnswer(q, Integer.parseInt(temp[temp.length-1]));
         }
+    }
+    
+    public int getAnswer(String query, int score) {
+        if (!map.containsKey(query)) return 0;
+        
+        ArrayList<Integer> arr = map.get(query);
+        int start = 0, end = arr.size()-1;
+        
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr.get(mid) < score) start = mid+1;
+            else end = mid-1;
+        }
+        
+        return arr.size() - start;
     }
     
     public int[] solution(String[] info, String[] query) {
         answer = new int[query.length];
         setInfo(info);
-        makeAnswer(query);
+        queryProcess(query);
         return answer;
     }
 }
