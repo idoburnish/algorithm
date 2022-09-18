492b5925c9499966de48cff075246acb4f649dd1
LEE DOYOON <doyun7433@naver.com>
2022-09-18 13:24:32 +0900

    [level 2] Title: 문자열 압축, Time: 10.57 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/README.md
+++ 프로그래머스/lv2/60057. 문자열 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] 문자열 압축 - 60057 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057#) 
 
 ### 성능 요약
 
-메모리: 88.2 MB, 시간: 8.82 ms
+메모리: 74.1 MB, 시간: 10.57 ms
 
 ### 구분
 
--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.java
@@ -3,35 +3,35 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         int answer = s.length();
-        
+
         for (int i=1; i<=s.length()/2; i++) {
             answer = Math.min(answer, splitString(s, i));
         }
-        
+
         return answer;
     }
-    
+
     public int splitString(String s, int num) {
         String compression = "";
-        
+
         int idx = 0;
         while (idx < s.length()) {
-            String repeat = s.substring(idx, idx + num > s.length() ? s.length() : idx + num);
+            String repeat = s.substring(idx, Math.min(idx + num, s.length()));
             int pointer = idx + num, cnt = 1;
-            
+
             while (pointer < s.length()) {
-                if (s.substring(pointer, pointer + num > s.length() ? s.length() : pointer + num).equals(repeat)) {
+                if (s.substring(pointer, Math.min(pointer + num, s.length())).equals(repeat)) {
                     pointer += num;
                     cnt++;
                 }
                 else break;
             }
-            
+
             if (cnt != 1) compression += cnt;
             compression += repeat;
             idx = pointer;
         }
-        
+
         return compression.length();
     }
 }
