748ea31fc117dbe1e71522291a1536939e6f8fba
LEE DOYOON <doyun7433@naver.com>
2022-10-06 00:32:16 +0900

    [Gold IV] Title: LCS 2, Time: 148 ms, Memory: 18284 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+// LCS 2
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] str1 = br.readLine().toCharArray();
+        char[] str2 = br.readLine().toCharArray();
+
+        int len1 = str1.length;
+        int len2 = str2.length;
+        int[][] dp = new int[len1+1][len2+1];
+
+        for (int i=1; i<=len1; i++) {
+            for (int j=1; j<=len2; j++) {
+                if (str1[i-1] == str2[j-1]) dp[i][j] = dp[i-1][j-1] + 1;
+                else dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+
+        System.out.println(dp[len1][len2]);
+        if (dp[len1][len2] > 0) {
+            StringBuilder sb = new StringBuilder();
+            int idx1 = len1, idx2 = len2;
+
+            while (idx1 != 0 && idx2 != 0) {
+                if (str1[idx1-1] == str2[idx2-1]) {
+                    sb.append(str1[idx1-1]);
+                    idx1--;
+                    idx2--;
+                }
+                else {
+                    if (dp[idx1][idx2] == dp[idx1][idx2-1]) idx2--;
+                    else idx1--;
+                }
+            }
+            System.out.println(sb.reverse());
+        }
+    }
+}
