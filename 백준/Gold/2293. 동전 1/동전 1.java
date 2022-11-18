158996768e55971aefdcd508e7649881d73071b1
LEE DOYOON <doyun7433@naver.com>
2022-11-18 23:51:02 +0900

    [Gold V] Title: 동전 1, Time: 128 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -1,24 +1,25 @@
 import java.io.*;
 import java.util.*;
 
+// 동전
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        int[] coin = new int[n];
-        for (int i=0; i<n; i++) coin[i] = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] dp = new int[K+1];
+        for (int i=0; i<N; i++) arr[i] = Integer.parseInt(br.readLine());
 
-        int[] dp = new int[k+1];
         dp[0] = 1;
-
-        for (int i=0; i<n; i++){
-            for (int j=coin[i]; j<=k; j++) {
-                dp[j] += dp[j - coin[i]];
+        for (int i=0; i<N; i++) {
+            for (int j=arr[i]; j<=K; j++) {
+                dp[j] += dp[j - arr[i]];
             }
         }
-        System.out.println(dp[k]);
+
+        System.out.println(dp[K]);
     }
 }
